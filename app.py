3c1b2dfec14a53517ff62705b704fb370cc0dea7
straightson <jason.h.pham@gmail.com>
2019-03-30 17:56:18 -0700

    update

--- app.py
+++ app.py
@@ -1,50 +1,73 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[7]:
+# In[6]:
 
 
-import pandas as pd
 import json
 import requests
 import time
 
-headers = ["Title"]
-for i in range(1,44):
-    headers.append(i)
-headers.append("Score")
-headers.append("Type")
+header = ['Title','type','genre','duration','source','producers','studios','score']
 
 
-# In[12]:
+# In[7]:
 
 
-df = pd.DataFrame(columns=headers)
-index = 13625
+import pygsheets
+#authorization
+gc = pygsheets.authorize(service_file='client_secret.json')
+
+#open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
+sh = gc.open('Myanimelist')
+
+wks = sh[0]
+wks.insert_rows(0,values=header)
+
+
+# In[16]:
+
+
+mal_index = 0
+spread_index = 1
 status_error = []
 
 
-# In[13]:
+# In[17]:
 
 
 while len(status_error) < 100:
-    index += 1
-    url = "https://api.jikan.moe/v3/anime/{}".format(index)
+    mal_index += 1
+    url = "https://api.jikan.moe/v3/anime/{}".format(mal_index)
     response = requests.get(url)
-    print(index, end=" ")
+    print(mal_index, end=" ")
     # Print the status code of the response.
     status = response.status_code
     
     if status == 200:
         data = response.json()
-        series = [0] * len(df.columns)
-        series[0] = data["title"]
-        series[-2] = data["score"]
-        series[-1] = data["type"]
-        for i in data["genres"]:
-            series[i['mal_id']]  = 1
-        df.loc[len(df.index)] = series
-        print(len(df.index), end =" ")
+        
+        producers = ""
+        for i in data['producers']:
+            producers += "{}, ".format(i['mal_id'])
+            
+        licensors = ""
+        for i in data['licensors']:
+            licensors += "{}, ".format(i['mal_id'])
+            
+        studios = ""
+        for i in data['studios']:
+            studios += "{}, ".format(i['mal_id'])
+            
+        genres = ""
+        for i in data['genres']:
+            genres += "{}, ".format(i['mal_id'])
+            
+        info = [data['title'],data['type'],genres,data['duration'],data['source'], producers, studios,data['score']]
+        
+        wks.insert_rows(spread_index,values=info)
+        spread_index += 1
+        
         status_error = []
     elif status == 404:
         status_error.append(status)
@@ -54,40 +77,4 @@ while len(status_error) < 100:
         continue
     print(status)
     time.sleep(1)
-
-
-# In[14]:
-
-
-df.tail()
-
-
-# In[ ]:
-
-
-len(status_error)
-
-
-# In[15]:
-
-
-# df.to_csv("anime {}.csv".format(index), encoding='utf-8',index=False)
-
-
-# In[15]:
-
-
-import pygsheets
-import pandas as pd
-#authorization
-gc = pygsheets.authorize(service_file='client_secret.json')
-
-#open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
-sh = gc.open('Myanimelist')
-
-#select the first sheet 
-wks = sh[0]
-
-#update the first sheet with df, starting at cell B2. 
-wks.set_dataframe(df,(1,1))
 
